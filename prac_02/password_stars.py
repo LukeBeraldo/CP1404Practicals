9182c7d5a42f1c934b9bdb5cfa25b7feeac859ae
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-09-30 10:56:17 +1000

    Revise functions and add docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,24 +1,26 @@
-"""Password Printer with error checking"""
+"""Get a valid password using minimum length and display with asterisks"""
 
 MINIMUM_LENGTH = 8
 
 
 def main():
-    """Get a valid password"""
-    password = get_password()
-    print_password(password)
+    """Get a password and print it using functions"""
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
 
 
-def print_password(password):
-    print(f"{'*' * len(password)}")
-
-
-def get_password():
+def get_password(minimum_length):
+    """Get password and check it meets a minimum length"""
     password = input("Enter password: ")
-    while len(password) < MINIMUM_LENGTH:
+    while len(password) < minimum_length:
         print("Password too short")
         password = input("Enter password: ")
     return password
+
+
+def print_asterisks(string):
+    """Print as many asterisks as there are in the string"""
+    print(f"{'*' * len(string)}")
 
 
 main()
