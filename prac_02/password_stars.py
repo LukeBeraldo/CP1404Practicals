2fde9ba4ced789dc7b9da5d0252055c97fce6a3c
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-09-30 10:42:16 +1000

    Add functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,24 @@
 """Password Printer with error checking"""
 
-minimum_length = 8
-password = input("Enter password: ")
-while len(password) < minimum_length:
-    print("Password too short")
+MINIMUM_LENGTH = 8
+
+
+def main():
+    """Get a valid password"""
+    password = get_password()
+    print_password(password)
+
+
+def print_password(password):
+    print(f"{'*' * len(password)}")
+
+
+def get_password():
     password = input("Enter password: ")
-print(f"{'*' * len(password)}")
+    while len(password) < MINIMUM_LENGTH:
+        print("Password too short")
+        password = input("Enter password: ")
+    return password
+
+
+main()
