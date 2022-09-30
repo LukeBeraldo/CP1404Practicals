56801c91a4c8dba73c88b33c87c4b9dcaa4084e2
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-09-30 12:31:48 +1000

    Add functions

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,25 +1,52 @@
 """
 CP1404/CP5632 - Practical
-Fixed program to determine score status using functions
+Score Menu Exercise
 """
+
+MENU_STRING = "(E)nter Score\n(R)esult\n(S)tars!\n(Q)uit"
 
 
 def main():
-    """Get a score and display its status """
-    score = float(input("Enter score: "))
-    print(determine_status(score))
+    """Display Menu with options to enter a score, determine a result or print stars"""
+    score = 0
+    print(MENU_STRING)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "E":
+            score = get_valid_score()
+        elif choice == "R":
+            print(f"Your score is {determine_status(score)}")
+        elif choice == "S":
+            print_asterisks(score)
+        else:
+            print("Invalid input")
+        print(MENU_STRING)
+        choice = input(">>> ").upper()
+    print("Bye")
+
+
+def get_valid_score():
+    """Get a valid score between 0 and 100"""
+    score = int(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Enter score: "))
+    return score
 
 
 def determine_status(score):
-    """Determine the status of score passed in"""
-    if score < 0 or score > 100:
-        print("Invalid score")
-    elif score >= 90:
+    """Determine the status of the score passed in"""
+    if score >= 90:
         return "Excellent"
     elif score >= 50:
         return "Passable"
     else:
         return "Bad"
+
+
+def print_asterisks(score):
+    """Print as many asterisks as the score"""
+    print(f"{'*' * score}")
 
 
 main()
