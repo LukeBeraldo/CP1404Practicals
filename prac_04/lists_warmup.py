969da8da0ed0660da65e3b1b9f7fce5a67ca8f64
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-10-12 19:27:22 +1000

    corrections

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,23 +1,21 @@
 """
 CP1404/CP5632 - Practical
-
+Basic list exercises
 """
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-# numbers[0] = 3
-# numbers[-1] = 2
-# numbers[3] = 1
-# numbers[:-1] = 3, 1, 4, 1, 5, 9
-# numbers[3:4] = 1
-# 5 in numbers = True
-# 7 in numbers = False
-# "3" in numbers = False
-# numbers + [6, 5, 3] = [3, 1, 4, 5, 9, 2, 6, 5, 3]
+# numbers[0] will give 3
+# numbers[-1] will give 2
+# numbers[3] will give 1
+# numbers[:-1] will give 3, 1, 4, 1, 5, 9
+# numbers[3:4] will give 1
+# 5 in numbers will give True
+# 7 in numbers will give False
+# "3" in numbers will give False
+# numbers + [6, 5, 3] will give [3, 1, 4, 5, 9, 2, 6, 5, 3]
 
-numbers[1] = "ten"  # 1.
+numbers[0] = "ten"  # 1.
 numbers[-1] = 1  # 2.
 print(numbers[2:])  # 3.
 print(9 in numbers)  # 4.
-
-# Check answers
