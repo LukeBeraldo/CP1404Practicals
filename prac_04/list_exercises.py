b01640f58d053108ac8812274347b2174f998709
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-10-12 20:41:09 +1000

    corrections

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,14 +6,23 @@ list_exercises using different functions
 NUMBER_OF_REPETITIONS = 5
 
 numbers = []
-
 for i in range(NUMBER_OF_REPETITIONS):
     number = int(input("Number: "))
     numbers.append(number)
+
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
-# check answers
+# 2. Username Check
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter username: ")
+if username in usernames:  # Do we not use a while loop because of problem domain?
+    print("Access denied")
+else:
+    print("Access granted")
