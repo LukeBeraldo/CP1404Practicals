d8b5c064130660f96ce62d4cbf289565c57438cc
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-10-18 14:43:15 +1000

    Change to EAFP from LBYL

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,13 +9,13 @@ STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Te
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(STATE_NAMES)
 
-for state_code in STATE_NAMES:
-    print(f"{state_code:3} is {STATE_NAMES[state_code]}")
+# for state_code in STATE_NAMES:
+#     print(f"{state_code:3} is {STATE_NAMES[state_code]}")
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in STATE_NAMES:
+    try:
         print(state_code, "is", STATE_NAMES[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
