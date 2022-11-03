84bc56a14923d04e4166d508b8b817fb1db5c483
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-11-03 14:22:49 +1000

    change to get colour by index not .get method.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,17 +3,17 @@ CP1404/CP5632 Practical
 Hexadecimal colour code dictionary
 """
 
-COLOUR_CODES = {"AbsoluteZero": "#0048ba", "AcidGreen": "#b0bf1a", "AliceBlue": "f0f8ff", "AlizarinCrimson": "#e32636",
-                "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc", "Aqua": "#00ffff",
-                "Aureolin": "#fdee00", "Beige": "#f5f5dc"}
+COLOUR_TO_CODE = {"AbsoluteZero": "#0048ba", "AcidGreen": "#b0bf1a", "AliceBlue": "f0f8ff",
+                  "AlizarinCrimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
+                  "Aqua": "#00ffff", "Aureolin": "#fdee00", "Beige": "#f5f5dc"}
 
-for state_code in STATE_NAMES:
-    print(f"{state_code:3} is {STATE_NAMES[state_code]}")
+for colour_name in COLOUR_TO_CODE:
+    print(f"{colour_name} is {COLOUR_TO_CODE[colour_name]}")
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
+colour_name = input("\nEnter colour name: ").title()
+while colour_name != " ":
     try:
-        print(state_code, "is", STATE_NAMES[state_code])
+        print(f"{colour_name} is, {COLOUR_TO_CODE[colour_name]}")
     except KeyError:
-        print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+        print("Invalid colour name")
+    colour_name = input("Enter colour name: ").title()
