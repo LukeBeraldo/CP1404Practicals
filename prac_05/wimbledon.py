f577a5f4c21e8cc4bcb2675908aa02534483a141
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-11-04 12:10:41 +1000

    main(), get records()

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,12 +6,21 @@ Actual:   40 minutes
 CHAMPION_INDEX = 2
 COUNTRIES_INDEX = 1
 
-record_entry = []
-with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
-    in_file.readline()  # Is this the only way to get rid of the first row?
-    for line in in_file:
-        parts = line.strip().split(",")
-        record_entry.append(parts)
+
+def main():
+    """Get record data and display information about Wimbledon champions and there countires """
+    get_records()
+
+
+def get_records():
+    """Get records from file and add to a list"""
+    record_entry = []
+    with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Is this the only way to get rid of the first row?
+        for line in in_file:
+            parts = line.strip().split(",")
+            record_entry.append(parts)
+    return record_entry
 
     champion_to_wins = {}
     countries = set()
@@ -29,4 +38,3 @@ with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
     print(f"These {len(countries)} countries have won Wimbledon")
     # couldn't figure out list comprehension, got it from solutions
     print(", ".join(country for country in sorted(countries)))
-
