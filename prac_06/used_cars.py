2020bfd39ed794529d2244c2af5e65da3c906a1f
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-11-06 19:47:28 +1000

    final steps done in used_cars.py. Docstrings

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,12 +9,12 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("my_car", 180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
     # 1. Create a new Car object called "limo" that is initialised with 100 units of fuel.
-    limo = Car(100)
+    limo = Car("limo", 100)
     # 2. Add 20 more units of fuel to this new car object using the add method.
     limo.add_fuel(20)
     # 3. Print the amount of fuel in the car.
@@ -25,6 +25,7 @@ def main():
     # 5. Using f-string formatting. Make it return a string in the following format:
     # Car, fuel=42, odometer=277
     print(limo)
+    # 6, 7 Done in car.py
 
 
 main()
