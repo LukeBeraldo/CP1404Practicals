d3ca74015ef72974b26046af23b7a7e42166dfb1
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-11-07 15:29:54 +1000

    make guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,10 @@
+"""CP1404/CP5632 Practical - Guitar class example."""
+
+
+class Guitar:
+    """SOMETING WONG"""
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
