684e299214822901e00067b34ba4f726413df244
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-11-07 15:37:08 +1000

    create other methods for guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,7 +4,20 @@
 class Guitar:
     """SOMETING WONG"""
 
-    def __init__(self, name="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0.0):
+        """"""
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return formatted string on guitar object attributes"""
+        return f"{self.name} {self.year} : {self.cost}"
+
+    def get_age(self):
+        """Returns how old guitar object is in years"""
+        return
+
+    def is_vintage(self):
+        """Returns True if guitar is 50 or more years old"""
+        return
