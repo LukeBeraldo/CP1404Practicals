0af377cc7b46f5020ec5e24f1d6380f7a5ebe0b9
LukeBeraldo <luke.beraldo@my.jcu.edu.au>
2022-11-07 14:52:29 +1000

    __str()__

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,13 +4,17 @@
 class ProgrammingLanguage:
     """Represent a programming language"""
 
-    def __init__(self, name="", typing="Static", reflection="No", year=""):
+    def __init__(self, name="", typing="Static", reflection=False, year=""):
         """Initialise a Car instance."""
 
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        """ """
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
         pass
